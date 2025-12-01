8183a4b7e946a145cd353979ebd12792b9449134
drodobyte <drodobyte@gmail.com>
2025-12-01 20:03:31 +0100

    minor class move

--- core/data-remote/src/main/java/com/drodobyte/core/data/remote/Api.kt
+++ core/data-remote/src/main/java/com/drodobyte/core/data/remote/Api.kt
@@ -1,5 +1,13 @@
 package com.drodobyte.core.data.remote
 
+import android.content.Context
+import com.drodobyte.data.remote.R
+import com.google.gson.Gson
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+import java.io.InputStreamReader
+
 internal interface Api {
 
     suspend fun foods(query: String, page: Int = 0, size: Int = 10): Response
@@ -23,4 +31,30 @@ data class Response(
             Protein(1003), Energy(1008)
         }
     }
+}
+
+internal class MockApi(private val context: Context) : Api {
+
+    override suspend fun foods(
+        query: String,
+        page: Int,
+        size: Int
+    ): Response =
+        with(context.resources) {
+            Gson().fromJson(
+                InputStreamReader(openRawResource(R.raw.food_api_mock_response)),
+                Response::class.java
+            )
+        }
+}
+
+internal interface RealApi : Api {
+
+    @Headers("X-Api-Key: DEMO_KEY")
+    @GET("foods/search")
+    override suspend fun foods(
+        @Query("query") query: String,
+        @Query("pageNumber") page: Int,
+        @Query("pageSize") size: Int,
+    ): Response
 }
