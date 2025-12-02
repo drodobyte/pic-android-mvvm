6639065797c5b77e40c9bf9444eee0c7cab84fd8
drodobyte <drodobyte@gmail.com>
2025-12-02 12:09:42 +0100

    warning

--- feature/intake/src/main/java/com/drodobyte/feature/intake/util/UtilCompose.kt
+++ feature/intake/src/main/java/com/drodobyte/feature/intake/util/UtilCompose.kt
@@ -18,14 +18,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.KeyboardType.Companion.Number
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
